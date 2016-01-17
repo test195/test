55e45b08bcc6842f522046e86e6433740a604712
druny <druny195@rambler.ru>
2016-01-17 22:30:51 +0200

    Добавили еще что-то

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Андрей
- * Date: 17.01.2016
- * Time: 22:17
- */
-phpinfo();
--- www/index.php
+++ www/index.php
@@ -1,8 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Андрей
- * Date: 17.01.2016
- * Time: 22:17
- */
-phpinfo();
